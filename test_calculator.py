be362debe9fe87d8120f9f392b2e29909741c7dc
Annabelle <annabelle.bosse.pro@gmail.com>
2024-04-16 10:19:19 +0200

    app.yml test erreur

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 from calculator import calculer_moyenne
 
 def test_calculer_moyenne():
-    assert calculer_moyenne([1, 2, 3, 4, 5]) == 3.0
+    assert calculer_moyenne([1, 2, 3, 4, 5]) == 5.0
     assert calculer_moyenne([10, 20, 30, 40, 50]) == 30.0
     assert calculer_moyenne([0, 0, 0, 0, 0]) == 0.0
